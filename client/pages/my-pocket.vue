78de66e993a2a831c3e91cdd643c5cbea10564bd
dubyclint <118350883+dubyclint@users.noreply.github.com>
2025-09-18 23:32:11 +0100

    Create my-pocket.vue

---
+++ client/pages/my-pocket.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <WalletOverview />
+    <CryptoSwap />
+  </div>
+</template>
+
+<script setup>
+import WalletOverview from '@/components/WalletOverview.vue'
+import CryptoSwap from '@/components/CryptoSwap.vue'
+</script>
