c52c9c168fb63a5054b908e0139338cca43ad1cf
dubyclint <118350883+dubyclint@users.noreply.github.com>
2025-09-18 20:26:40 +0100

    Create post routes.js

---
+++ post routes.js
@@ -0,0 +1,9 @@
+// server/routes/postRoutes.js
+const express = require('express')
+const router = express.Router()
+const { createPost, likePost } = require('../controllers/postController')
+
+router.post('/create', createPost)
+router.post('/like', likePost)
+
+module.exports = router
