6cf2c8ebe39fd51c090347e0c17035291e9d828b
dubyclint <118350883+dubyclint@users.noreply.github.com>
2025-09-18 23:31:17 +0100

    Create cross-meet.vue

---
+++ client/pages/cross-meet.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <MatchRequestForm />
+    <MatchResults />
+  </div>
+</template>
+
+<script setup>
+import MatchRequestForm from '@/components/MatchRequestForm.vue'
+import MatchResults from '@/components/MatchResults.vue'
+</script>
