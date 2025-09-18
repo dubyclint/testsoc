ea828cbd9894b634bb7fc86a5def4e97ea4fdb82
dubyclint <118350883+dubyclint@users.noreply.github.com>
2025-09-18 23:44:19 +0100

    Create gift.vue

---
+++ client/pages/gift.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <GiftSummary />
+    <GiftHistory />
+  </div>
+</template>
+
+<script setup>
+import GiftSummary from '@/components/GiftSummary.vue'
+import GiftHistory from '@/components/GiftHistory.vue'
+</script>
