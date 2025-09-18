d975eb89dfc1b3cd80f6bd93667224b59cf52d4b
dubyclint <118350883+dubyclint@users.noreply.github.com>
2025-09-18 21:24:22 +0100

    Create universe.vue

---
+++ client/pages/universe.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <PalRequest />
+    <PalList />
+    <UniverseChat />
+  </div>
+</template>
+
+<script setup>
+import PalRequest from '@/components/PalRequest.vue'
+import PalList from '@/components/PalList.vue'
+import UniverseChat from '@/components/UniverseChat.vue'
+</script>
