2a251db47e401488e1e02d3e4b62b9f2036701a4
dubyclint <118350883+dubyclint@users.noreply.github.com>
2025-09-18 20:58:53 +0100

    Create p2p.vue

---
+++ client/pages/p2p.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <P2PTradeForm />
+    <TradeChat />
+    <EscrowStatus />
+  </div>
+</template>
+
+<script setup>
+import P2PTradeForm from '@/components/P2PTradeForm.vue'
+import TradeChat from '@/components/TradeChat.vue'
+import EscrowStatus from '@/components/EscrowStatus.vue'
+</script>
