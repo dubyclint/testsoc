c189b1400c009579272eacb941235e73ff45b936
dubyclint <118350883+dubyclint@users.noreply.github.com>
2025-09-19 00:12:14 +0100

    Create admin.vue

---
+++ client/pages/admin.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <AdminUserList />
+    <AdminPostModeration />
+    <AdminMatchApproval />
+    <AdminEscrowControl />
+    <AdminFeeManager />
+  </div>
+</template>
+
+<script setup>
+import AdminUserList from '@/components/AdminUserList.vue'
+import AdminPostModeration from '@/components/AdminPostModeration.vue'
+import AdminMatchApproval from '@/components/AdminMatchApproval.vue'
+import AdminEscrowControl from '@/components/AdminEscrowControl.vue'
+import AdminFeeManager from '@/components/AdminFeeManager.vue'
+</script>
