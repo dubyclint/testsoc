ed2c1e9e5125dc2099dceedcfff05daec3f8d467
dubyclint <118350883+dubyclint@users.noreply.github.com>
2025-09-19 19:13:09 +0100

    Create [id].vue

---
+++ client/pages/profile/[id].vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="profile-header">
+    <h2>{{ user.name }} <VerifiedBadgeIcon :userId="user.id" /></h2>
+    <!-- other profile info -->
+  </div>
+</template>
+
+<script setup>
+import VerifiedBadgeIcon from '@/components/VerifiedBadgeIcon.vue'
+const route = useRoute()
+const user = await fetchUser(route.params.id) // your existing logic
+</script>
