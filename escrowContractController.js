c5a69dda1d2f52481e5018aa837c974bdfae95aa
dubyclint <118350883+dubyclint@users.noreply.github.com>
2025-09-19 03:05:23 +0100

    Update escrowContractController.js

--- escrowContractController.js
+++ escrowContractController.js
@@ -1,2 +1,7 @@
 const io = req.app.get('io')
 io.emit('escrow:update', { dealId, action: 'released' }) // or 'refunded' or 'created'
+io.emit('escrow:userUpdate', {
+  dealId,
+  action: 'released', // or 'refunded', 'created'
+  userId: buyerId // or sellerId depending on context
+})
