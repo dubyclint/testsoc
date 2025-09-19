d2f953864f51ee61e389ed71a980dd181e0a4f0a
dubyclint <118350883+dubyclint@users.noreply.github.com>
2025-09-19 19:21:00 +0100

    Create ChatMessage.vue

---
+++ client/components/ChatMessage.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="chat-message">
+    <span class="sender">{{ message.sender.name }}</span>
+    <VerifiedBadgeIcon :userId="message.sender.id" />
+    <p>{{ message.text }}</p>
+  </div>
+</template>
+
+<script setup>
+import VerifiedBadgeIcon from '@/components/VerifiedBadgeIcon.vue'
+defineProps({ message: Object })
+</script>
