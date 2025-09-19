11224921b63c6f5a8b133e8b20c56c9f1e93b707
dubyclint <118350883+dubyclint@users.noreply.github.com>
2025-09-19 01:01:22 +0100

    Create Notification.js

---
+++ Notification.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+
+const notificationSchema = new mongoose.Schema({
+  userId: String,
+  type: String,
+  message: String,
+  isRead: { type: Boolean, default: false },
+  timestamp: { type: Date, default: Date.now }
+})
+
+module.exports = mongoose.model('Notification', notificationSchema)
