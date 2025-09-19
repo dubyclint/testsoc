35a454c6b508936b804d3ffdb7189d5c59371946
dubyclint <118350883+dubyclint@users.noreply.github.com>
2025-09-19 02:15:56 +0100

    Create admin-escrow.vue

---
+++ admin-escrow.vue
@@ -0,0 +1,11 @@
+<script setup>
+import EscrowAdminActions from '@/components/EscrowAdminActions.vue'
+import EscrowPendingTable from '@/components/EscrowPendingTable.vue'
+</script>
+
+<template>
+  <div>
+    <EscrowAdminActions />
+    <EscrowPendingTable />
+  </div>
+</template>
