97674459f6daca9ed36e5ce955b252c9f7539d25
dubyclint <118350883+dubyclint@users.noreply.github.com>
2025-09-19 20:46:53 +0100

    Create ranks.vue

---
+++ pages/admin/ranks.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="admin-ranks">
+    <RankAdminPanel />
+  </div>
+</template>
+
+<script setup>
+import RankAdminPanel from '@/components/RankAdminPanel.vue'
+definePageMeta({ middleware: ['admin-auth'] })
+</script>
