66796954bd4443405424eeab8a39046d7c3f0afd
dubyclint <118350883+dubyclint@users.noreply.github.com>
2025-09-20 05:48:56 +0100

    Create [id].vue

---
+++ pages/profile/[id].vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <UserProfile :id="id" />
+    <AdSlot page="Profile" />
+  </div>
+</template>
+
+<script setup>
+import { useRoute } from 'vue-router'
+import UserProfile from '@/components/UserProfile.vue'
+import AdSlot from '@/components/AdSlot.vue'
+
+const id = useRoute().params.id
+</script>
