db82c19dc41279c1dc4dde41b429b1072f601eb5
dubyclint <118350883+dubyclint@users.noreply.github.com>
2025-09-20 06:12:07 +0100

    Create [id].vue

---
+++ pages/post/[id].vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <PostDetail :id="id" />
+    <AdSlot page="Post Detail" />
+  </div>
+</template>
+
+<script setup>
+import { useRoute } from 'vue-router'
+import PostDetail from '@/components/PostDetail.vue'
+import AdSlot from '@/components/AdSlot.vue'
+
+const id = useRoute().params.id
+</script>
