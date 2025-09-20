1cb98815d325e11b6d9365fcbb37e88a21905b82
dubyclint <118350883+dubyclint@users.noreply.github.com>
2025-09-20 06:28:07 +0100

    Create ConversionTracker.vue

---
+++ client/components/ConversionTracker.vue
@@ -0,0 +1,12 @@
+<script setup>
+const props = defineProps({ adId: String, action: String })
+
+async function trackConversion() {
+  await fetch('/api/ads/conversion', {
+    method: 'POST',
+    body: JSON.stringify({ adId: props.adId, action: props.action }),
+    headers: { 'Content-Type': 'application/json' }
+  })
+}
+trackConversion()
+</script>
