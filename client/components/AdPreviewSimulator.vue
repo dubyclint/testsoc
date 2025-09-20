8647ef86efc2cdc3e24e0ddf3603a6d1ee138a2a
dubyclint <118350883+dubyclint@users.noreply.github.com>
2025-09-20 05:00:29 +0100

    Create AdPreviewSimulator.vue

---
+++ client/components/AdPreviewSimulator.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h4>Preview</h4>
+    <div class="preview mobile">
+      <slot name="mobile" />
+    </div>
+    <div class="preview desktop">
+      <slot name="desktop" />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.preview {
+  border: 1px solid #ccc;
+  margin: 1rem 0;
+  padding: 1rem;
+}
+.mobile {
+  width: 375px;
+}
+.desktop {
+  width: 1024px;
+}
+</style>
