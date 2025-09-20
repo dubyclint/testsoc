60b5ce32579f57fca20ce32bd64276524c2a02a2
dubyclint <118350883+dubyclint@users.noreply.github.com>
2025-09-20 05:47:05 +0100

    Create feed.vue

---
+++ pages/feed.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <FeedPosts />
+    <AdSlot page="Home Feed" />
+  </div>
+</template>
+
+<script setup>
+import FeedPosts from '@/components/FeedPosts.vue'
+import AdSlot from '@/components/AdSlot.vue'
+</script>
