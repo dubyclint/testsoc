c5da3691a9a8e9f57097ede60c523b73635af91e
dubyclint <118350883+dubyclint@users.noreply.github.com>
2025-09-20 06:13:26 +0100

    Create trade.vue

---
+++ pages/trade.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <TradeListings />
+    <AdSlot page="Trade Listings" />
+  </div>
+</template>
+
+<script setup>
+import TradeListings from '@/components/TradeListings.vue'
+import AdSlot from '@/components/AdSlot.vue'
+</script>
