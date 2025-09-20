acb0089095b3f19abad12b4d1f47557a16b3fc22
dubyclint <118350883+dubyclint@users.noreply.github.com>
2025-09-20 06:12:46 +0100

    Create chat.vue

---
+++ pages/chat.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="chat-layout">
+    <ChatWindow />
+    <aside class="chat-sidebar">
+      <AdSlot page="Chat Sidebar" />
+    </aside>
+  </div>
+</template>
+
+<script setup>
+import ChatWindow from '@/components/ChatWindow.vue'
+import AdSlot from '@/components/AdSlot.vue'
+</script>
+
+<style scoped>
+.chat-layout {
+  display: flex;
+}
+.chat-sidebar {
+  width: 300px;
+  padding-left: 1rem;
+}
+</style>
