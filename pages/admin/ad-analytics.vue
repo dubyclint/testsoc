65ad706d85cb18be9d0a06a31daea00512745e0e
dubyclint <118350883+dubyclint@users.noreply.github.com>
2025-09-20 06:18:22 +0100

    Create ad-analytics.vue

---
+++ pages/admin/ad-analytics.vue
@@ -0,0 +1,8 @@
+<template>
+  <AdminAdAnalytics />
+</template>
+
+<script setup>
+import AdminAdAnalytics from '@/components/AdminAdAnalytics.vue'
+definePageMeta({ middleware: ['admin-auth'] })
+</script>
