c26618fb56db8e10069d9c6e5f3c3285ea07ee37
dubyclint <118350883+dubyclint@users.noreply.github.com>
2025-09-20 05:47:46 +0100

    Create explore.vue

---
+++ pages/explore.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <ExploreGrid />
+    <AdSlot page="Explore" />
+  </div>
+</template>
+
+<script setup>
+import ExploreGrid from '@/components/ExploreGrid.vue'
+import AdSlot from '@/components/AdSlot.vue'
+</script>
