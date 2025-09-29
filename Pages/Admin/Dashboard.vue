47c0c2692d8fb6ab3880f7c3809c82fe0be51685
dubyclint <118350883+dubyclint@users.noreply.github.com>
2025-09-29 22:13:56 +0100

    Create Dashboard.vue

---
+++ Pages/Admin/Dashboard.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h2>Admin Dashboard</h2>
+    <ul>
+      <li><NuxtLink to="/admin/users">Manage Users</NuxtLink></li>
+      <li><NuxtLink to="/admin/content">Moderate Content</NuxtLink></li>
+      <li><NuxtLink to="/admin/stats">View Analytics</NuxtLink></li>
+    </ul>
+  </div>
+</template>
