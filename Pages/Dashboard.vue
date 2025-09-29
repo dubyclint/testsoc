2d85034a82af1329c6b60e76702ec775803672c2
dubyclint <118350883+dubyclint@users.noreply.github.com>
2025-09-29 22:06:05 +0100

    Create Dashboard.vue

---
+++ Pages/Dashboard.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <Navbar />
+    <h2>Welcome back, {{ user.name }}</h2>
+    <p>Your personalized feed is loading...</p>
+  </div>
+</template>
+
+<script setup>
+const user = useUser() // composable or store
+</script>
