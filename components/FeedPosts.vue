447dd79deadc56965471367ff981e3af110f0905
dubyclint <118350883+dubyclint@users.noreply.github.com>
2025-10-01 23:32:32 +0100

    Create FeedPosts.vue

---
+++ components/FeedPosts.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="feed-posts">
+    <h2>Latest Posts</h2>
+    <ul>
+      <li v-for="post in posts" :key="post.id">{{ post.content }}</li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+const posts = ref([
+  { id: 1, content: 'Welcome to Socialverse!' },
+  { id: 2, content: 'This is your personalized feed.' }
+])
+</script>
+
+<style scoped>
+.feed-posts {
+  padding: 1rem;
+}
+</style>
