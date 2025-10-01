29943075375ec1086dfdcbf49da02b74562d950c
dubyclint <118350883+dubyclint@users.noreply.github.com>
2025-10-02 00:12:44 +0100

    Update FeedPosts.vue

--- components/FeedPosts.vue
+++ components/FeedPosts.vue
@@ -4,14 +4,33 @@
     <ul>
       <li v-for="post in posts" :key="post.id">{{ post.content }}</li>
     </ul>
+    <button @click="loadMore">Load More</button>
   </div>
 </template>
 
 <script setup>
-const posts = ref([
-  { id: 1, content: 'Welcome to Socialverse!' },
-  { id: 2, content: 'This is your personalized feed.' }
-])
+import { gun } from '~/gundb/client'
+const posts = ref([])
+const page = ref(1)
+
+async function fetchPage() {
+  const { data } = await useFetch(`/api/posts?page=${page.value}`)
+  if (data.value) posts.value.push(...data.value)
+}
+
+function loadMore() {
+  page.value++
+  fetchPage()
+}
+
+onMounted(() => {
+  fetchPage()
+  gun.get('socialverse-feed').map().on((data, key) => {
+    if (!posts.value.find(p => p.id === key)) {
+      posts.value.unshift({ id: key, ...data })
+    }
+  })
+})
 </script>
 
 <style scoped>
