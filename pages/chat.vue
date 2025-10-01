012003fd90d5ac149de5f2c94d776544d6a1036c
dubyclint <118350883+dubyclint@users.noreply.github.com>
2025-10-01 23:26:20 +0100

    Update chat.vue

--- pages/chat.vue
+++ pages/chat.vue
@@ -1,23 +1,7 @@
 <template>
-  <div class="chat-layout">
-    <ChatWindow />
-    <aside class="chat-sidebar">
-      <AdSlot page="Chat Sidebar" />
-    </aside>
-  </div>
+  <ChatWindow />
 </template>
 
 <script setup>
-import ChatWindow from '@/components/ChatWindow.vue'
-import AdSlot from '@/components/AdSlot.vue'
+import ChatWindow from '~/components/ChatWindow.vue'
 </script>
-
-<style scoped>
-.chat-layout {
-  display: flex;
-}
-.chat-sidebar {
-  width: 300px;
-  padding-left: 1rem;
-}
-</style>
