51fedcac8548502a9c393650e78219de5a1e6a77
dubyclint <118350883+dubyclint@users.noreply.github.com>
2025-10-02 11:19:38 +0100

    Update eslintrc.js

--- eslintrc.js
+++ eslintrc.js
@@ -1,1 +1,14 @@
+// .eslintrc.js
+module.exports = {
+  root: true,
+  extends: [
+    '@nuxtjs/eslint-config-typescript',
+    'plugin:vue/vue3-recommended',
+    'plugin:prettier/recommended'
+  ],
+  rules: {
+    'semi': ['error', 'always'],
+    'vue/multi-word-component-names': 0
+  }
+};
 
