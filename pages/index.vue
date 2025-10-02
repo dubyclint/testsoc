be3a6740236bcdf3320acd3892a3d26213058e24
dubyclint <118350883+dubyclint@users.noreply.github.com>
2025-10-02 12:40:07 +0100

    Create index.vue

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<script setup>
+const { data: posts } = await useFetch('/api/posts');
+</script>
+
+<template>
+  <div>
+    <h1>Posts</h1>
+    <ul>
+      <li v-for="post in posts" :key="post.id">{{ post.title }}</li>
+    </ul>
+  </div>
+</template>
