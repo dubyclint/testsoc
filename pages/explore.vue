6d0e8a83fc5fc14e39d185b52f93d590ac4a9756
dubyclint <118350883+dubyclint@users.noreply.github.com>
2025-10-02 23:40:40 +0100

    Update explore.vue

--- pages/explore.vue
+++ pages/explore.vue
@@ -1,11 +1,11 @@
+<script setup>
+import ExploreGrid from '~/components/ui/ExploreGrid.vue'
+import AdSlot from '~/components/ui/AdSlot.vue'
+</script>
+
 <template>
   <div>
     <ExploreGrid />
     <AdSlot page="Explore" />
   </div>
 </template>
-
-<script setup>
-import ExploreGrid from '~/ExploreGrid.vue'
-import AdSlot from '~/AdSlot.vue'
-</script>
