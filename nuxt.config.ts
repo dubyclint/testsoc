bd08036e75d941d9255fcaf7369690be242b0be4
dubyclint <118350883+dubyclint@users.noreply.github.com>
2025-10-02 18:17:24 +0100

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,8 +2,9 @@ export default defineNuxtConfig({
   modules: [],
   runtimeConfig: {
     public: {
-      supabaseUrl: process.env. https://cvzrhucbvezqwbesthek.supabase.co,
-      supabaseKey: process.env.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN2enJodWNidmV6cXdiZXN0aGVrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTkzNzgzMjYsImV4cCI6MjA3NDk1NDMyNn0.3k5QE5wTb0E52CqNxwt_HaU9jUGDlYsHWuP7rQVjY4I
+      supabaseUrl: process.env.SUPABASE_URL,
+      supabaseKey: process.env.SUPABASE_KEY
     }
   }
 });
+
