c0438f8a3462a67509d5a9dc62677ead3f134bab
dubyclint <118350883+dubyclint@users.noreply.github.com>
2025-10-03 00:20:01 +0100

    Update trade.vue

--- pages/trade.vue
+++ pages/trade.vue
@@ -6,7 +6,7 @@
 </template>
 
 <script setup>
-import TradeListings from '@/components/TradeListings.vue';
-import AdSlot from '@/components/AdSlot.vue';
+import TradeListings from '~/components/TradeListings.vue';
+import AdSlot from '~/components/ui/AdSlot.vue';
 </script>
 
