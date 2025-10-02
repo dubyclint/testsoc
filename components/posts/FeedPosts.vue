29a59b29537124c8472a9f1e627f9f748cd28bf8
dubyclint <118350883+dubyclint@users.noreply.github.com>
2025-10-02 23:42:11 +0100

    Create FeedPosts.vue

---
+++ components/posts/FeedPosts.vue
@@ -0,0 +1,9 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+import { gun } from '~/gundb/client'
+import CreatePost from '~/components/posts/CreatePost.vue'
+import MarkdownIt from 'markdown-it'
+import emojione from 'emojione'
+
+// Rest of the code remains the same
+</script>
