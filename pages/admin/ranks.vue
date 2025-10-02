95aa0f04c58a39103a2877eef4f4f8151a4f5d8d
dubyclint <118350883+dubyclint@users.noreply.github.com>
2025-10-02 21:29:42 +0100

    Update ranks.vue

--- pages/admin/ranks.vue
+++ pages/admin/ranks.vue
@@ -1,10 +1,4 @@
-<template>
-  <div class="admin-ranks">
-    <RankAdminPanel />
-  </div>
-</template>
-
 <script setup>
-import RankAdminPanel from '@/components/RankAdminPanel.vue'
-definePageMeta({ middleware: ['admin-auth'] })
+import RankAdminPanel from '@/components/RankAdminPanel.vue';
+definePageMeta({ middleware: ['admin-auth'] });
 </script>
