db9e4b1a716976491c1e080589aa8a06b2535725
dubyclint <118350883+dubyclint@users.noreply.github.com>
2025-10-02 23:50:54 +0100

    Create index.ts

---
+++ components/index.ts
@@ -0,0 +1,26 @@
+// Auth Components
+export { default as AuthForm } from './auth/AuthForm.vue'
+export { default as EditProfile } from './auth/EditProfile.vue'
+
+// Chat Components
+export { default as ChatBox } from './chat/ChatBox.vue'
+export { default as ChatWindow } from './chat/ChatWindow.vue'
+export { default as GroupChat } from './chat/GroupChat.vue'
+export { default as CreateGroup } from './chat/CreateGroup.vue'
+
+// Post Components
+export { default as CreatePost } from './posts/CreatePost.vue'
+export { default as FeedPosts } from './posts/FeedPosts.vue'
+export { default as PostDetail } from './posts/PostDetail.vue'
+
+// UI Components
+export { default as AdSlot } from './ui/AdSlot.vue'
+export { default as ExploreGrid } from './ui/ExploreGrid.vue'
+
+// Admin Components
+export { default as AdminPanel } from './admin/AdminPanel.vue'
+export { default as AdminAdAnalytics } from './admin/AdminAdAnalytics.vue'
+
+// Layout Components
+export { default as Header } from './layout/Header.vue'
+export { default as Footer } from './layout/Footer.vue'
