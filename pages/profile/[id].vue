261a4a03af2401f40af5076558a7539d35858265
dubyclint <118350883+dubyclint@users.noreply.github.com>
2025-10-02 21:37:04 +0100

    Update [id].vue

--- pages/profile/[id].vue
+++ pages/profile/[id].vue
@@ -1,14 +1,7 @@
-<template>
-  <div>
-    <UserProfile :id="id" />
-    <AdSlot page="Profile" />
-  </div>
-</template>
-
 <script setup>
-import { useRoute } from 'vue-router'
-import UserProfile from '@/components/UserProfile.vue'
-import AdSlot from '@/components/AdSlot.vue'
+import { useRoute } from 'vue-router';
+import UserProfile from '@/components/UserProfile.vue';
+import AdSlot from '@/components/AdSlot.vue';
 
-const id = useRoute().params.id
+const id = useRoute().params.id;
 </script>
