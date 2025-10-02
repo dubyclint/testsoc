b9cabedb5730c89b52caa4ba769128bec626f0d9
dubyclint <118350883+dubyclint@users.noreply.github.com>
2025-10-02 02:56:31 +0100

    Create [id].vue

---
+++ pages/groups/[id].vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="group-page">
+    <CreateGroup />
+    <GroupChat :groupId="groupId" />
+    <AdminPanel />
+  </div>
+</template>
+
+<script setup>
+import { useRoute } from '#app'
+import GroupChat from '~/components/GroupChat.vue'
+import CreateGroup from '~/components/CreateGroup.vue'
+import AdminPanel from '~/components/AdminPanel.vue'
+
+const route = useRoute()
+const groupId = route.params.id as string
+</script>
+
+<style scoped>
+.group-page {
+  display: grid;
+  gap: 1rem;
+  max-width: 800px;
+  margin: 0 auto;
+}
+</style>
