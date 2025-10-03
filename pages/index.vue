4dce7c9039e8aea6aa5ab825105e74b54e89292b
dubyclint <118350883+dubyclint@users.noreply.github.com>
2025-10-03 02:51:42 +0100

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -5,8 +5,9 @@ const { data: posts } = await useFetch('/api/posts');
 <template>
   <div>
     <h1>Posts</h1>
-    <ul>
+    <ul v-if="posts && posts.length">
       <li v-for="post in posts" :key="post.id">{{ post.title }}</li>
     </ul>
+    <p v-else>No posts available</p>
   </div>
 </template>
