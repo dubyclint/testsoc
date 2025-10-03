d99fd6d1e77b0bb14a3031eef6f4ebf8f16b4dc5
dubyclint <118350883+dubyclint@users.noreply.github.com>
2025-10-03 03:00:15 +0100

    Update inbox.vue

--- pages/inbox.vue
+++ pages/inbox.vue
@@ -1,8 +1,12 @@
 <template>
-  <UserInbox />
+  <div>
+    <h1>Inbox</h1>
+    <p>Your messages will appear here</p>
+    <!-- <UserInbox /> -->
+  </div>
 </template>
 
 <script setup>
-import UserInbox from '~/components/UserInbox.vue';
+// import UserInbox from '~/components/UserInbox.vue'; // Component may not exist
 </script>
 
