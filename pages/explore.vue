e0a90f1d50b7951f88fd19a6dfa9e6390114d311
dubyclint <118350883+dubyclint@users.noreply.github.com>
2025-10-03 02:57:45 +0100

    Update explore.vue

--- pages/explore.vue
+++ pages/explore.vue
@@ -1,11 +1,14 @@
 <script setup>
-import ExploreGrid from '~/components/ui/ExploreGrid.vue';
-import AdSlot from '~/components/ui/AdSlot.vue';
+// import ExploreGrid from '~/components/ui/ExploreGrid.vue'; // Component may not exist
+// import AdSlot from '~/components/ui/AdSlot.vue'; // Component may not exist
 </script>
 
 <template>
   <div>
-    <ExploreGrid />
-    <AdSlot page="Explore" />
+    <h1>Explore</h1>
+    <p>Explore content will be displayed here</p>
+    <!-- <ExploreGrid /> -->
+    <!-- <AdSlot page="Explore" /> -->
   </div>
 </template>
+
