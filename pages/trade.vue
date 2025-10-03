0ba542f7bae353cebc2db44c55ae477af1593b76
dubyclint <118350883+dubyclint@users.noreply.github.com>
2025-10-03 02:59:03 +0100

    Update trade.vue

--- pages/trade.vue
+++ pages/trade.vue
@@ -1,12 +1,15 @@
 <template>
   <div>
-    <TradeListings />
-    <AdSlot page="Trade Listings" />
+    <h1>Trade</h1>
+    <p>Trading functionality will be available here</p>
+    <!-- <TradeListings /> -->
+    <!-- <AdSlot page="Trade Listings" /> -->
   </div>
 </template>
 
 <script setup>
-import TradeListings from '~/components/TradeListings.vue';
-import AdSlot from '~/components/ui/AdSlot.vue';
+// import TradeListings from '~/components/TradeListings.vue'; // Component may not exist
+// import AdSlot from '~/components/ui/AdSlot.vue'; // Component may not exist
 </script>
+
 
