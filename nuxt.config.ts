c5ed83e030400274813b1850d274c928c83fcc73
dubyclint <118350883+dubyclint@users.noreply.github.com>
2025-10-05 02:04:08 +0100

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,7 +12,7 @@ export default defineNuxtConfig({
     public: {
       supabaseUrl: process.env.https://cvzrhucbvezqwbesthek.supabase.co,
       supabaseAnonKey: process.env.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN2enJodWNidmV6cXdiZXN0aGVrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTkzNzgzMjYsImV4cCI6MjA3NDk1NDMyNn0.3k5QE5wTb0E52CqNxwt_HaU9jUGDlYsHWuP7rQVjY4I,
-      appBaseUrl: process.env.NUXT_APP_BASE_URL || 'https://testwe.zeabur.app,
+      appBaseUrl: process.env.NUXT_APP_BASE_URL || 'https://testwe.zeabur.app',
       appName: process.env.NUXT_APP_NAME || 'SocialVerse'
     }
   },
