50a740abb7c3ecb5544a8af2d525ff2b65e1b033
dubyclint <118350883+dubyclint@users.noreply.github.com>
2025-10-06 20:05:50 +0100

    Delete utils/supabase.js

--- utils/supabase.js
+++
@@ -1,7 +0,0 @@
-// utils/supabase.js
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = process.env.SUPABASE_URL
-const supabaseAnonKey = process.env.SUPABASE_ANON_KEY
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
