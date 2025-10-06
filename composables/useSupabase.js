bce181d1cbec10c04a47b02231b406c62e9b06ca
dubyclint <118350883+dubyclint@users.noreply.github.com>
2025-10-06 18:09:19 +0100

    Update useSupabase.js

--- composables/useSupabase.js
+++ composables/useSupabase.js
@@ -1,8 +1,4 @@
 // composables/useSupabase.js
 export const useSupabase = () => {
-  if (process.client) {
-    const { $supabase } = useNuxtApp()
-    return $supabase
-  }
-  return null
+  return useSupabaseClient()
 }
